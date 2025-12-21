04b453daf45c373a69e95d8f3d7c67a4a031b3fc
Luis Vasquez <ing.luisvasquez1311@GMAIL.COM>
2025-12-20 20:10:26 -0400

    Corregir apiVersion de Stripe para compatibilidad con Render

--- web/lib/stripe.ts
+++ web/lib/stripe.ts
@@ -3,5 +3,5 @@ import Stripe from 'stripe';
 const apiKey = process.env.STRIPE_SECRET_KEY || '';
 
 export const stripe = new Stripe(apiKey, {
-    apiVersion: '2025-12-15.clover', // Latest stable API version
+    apiVersion: '2025-02-24.acacia', // Latest stable API version
 });
